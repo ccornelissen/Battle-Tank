aa21d6d66e36aba567b887d5a1e8a6296b422f8d
CODYASUS\Cody <codycornelissen@gmail.com>
2017-01-06 20:04:53 -0800

    Made it so the AI tank will move towards the player, and will rotate to face them.

--- Project/BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ Project/BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -6,19 +6,36 @@
 
 void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-	if (!LeftTrackToSet || !RightTrackToSet)
+	if (!LeftTrackToSet || !RightTrackToSet) //null check
 	{
 		UE_LOG(LogTemp, Error, TEXT("Tracks returning null on the Tank Movement Component"));
 		return;
 	}
 
+	//Variables passed through tank blueprint
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 }
 
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
 {
-	UE_LOG(LogTemp, Warning, TEXT("%s moving towards %s"), *GetOwner()->GetName(), *MoveVelocity.ToString());
+	//Vector in the direction the AI would like to move
+	FVector AINormalVector = MoveVelocity.GetSafeNormal();
+
+	//Getting the current forward direction of the AI controlled tank
+	FVector TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
+
+	//Getting the dot product to find how parallel the 2 vectors are, which will drive the movement velocity.
+	float fForward = FVector::DotProduct(AINormalVector, TankForward);
+
+	//Telling the tank to move with our found float
+	IntendMoveForward(fForward);
+
+	//Getting the cross product to find the tanks "perpandicularity"
+	FVector TankCross = FVector::CrossProduct(AINormalVector, TankForward);
+
+	//Rotate based on the Z
+	IntendRotate(TankCross.Z);
 }
 
 void UTankMovementComponent::IntendMoveForward(float Speed)
@@ -28,6 +45,7 @@ void UTankMovementComponent::IntendMoveForward(float Speed)
 		return;
 	}
 
+	//Applying speed equally to both tracks as the player wants to move forward.
 	LeftTrack->MoveTrack(Speed);
 	RightTrack->MoveTrack(Speed);
 
@@ -41,6 +59,7 @@ void UTankMovementComponent::IntendRotate(float Speed)
 		return;
 	}
 
+	//Rotating to the right, right track moves backwards, left forwards, rotating the tank
 	LeftTrack->MoveTrack(Speed);
 	RightTrack->MoveTrack(-Speed);
 
